c657a54a8eed7576e571c77c6ce34ea7b42c4d3b
Marton Horvath <17813215+horvathmarton@users.noreply.github.com>
2021-12-08 16:46:16 +0100

    feat: enhance admin site for stocks

--- apps/stocks/admin.py
+++ apps/stocks/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Stock
 
-admin.site.register(Stock)
+
+class StockAdmin(admin.ModelAdmin):
+    list_display = ("ticker", "name", "sector", "active", "created_at", "updated_at")
+    list_filter = ["sector", "active"]
+    search_fields = ["name"]
+
+
+admin.site.register(Stock, StockAdmin)
