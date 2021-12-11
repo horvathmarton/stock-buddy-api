91e4b2ce83a529ec3a8bb19dbcdbfb55a8e52c36
Marton Horvath <17813215+horvathmarton@users.noreply.github.com>
2021-12-11 08:57:18 +0100

    fix: configure CORS headers

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework.authtoken",
+    "corsheaders",
     "core",
     "apps.raw_data",
     "apps.stocks",
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -129,3 +131,7 @@ REST_FRAMEWORK = {
 }
 
 TEST_RUNNER = "core.test.runner.PostgresSchemaTestRunner"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+]
