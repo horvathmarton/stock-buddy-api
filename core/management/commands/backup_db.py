835043e84bedc3242e67a8253350003eba9cb153
Marton Horvath <17813215+horvathmarton@users.noreply.github.com>
2022-01-02 21:03:52 +0100

    build: add database backup command

---
+++ core/management/commands/backup_db.py
@@ -0,0 +1,16 @@
+"""Creates a backup from the active database."""
+
+from os import getenv, system
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Custom command to backup the database."""
+
+    help = "Create a backup from the database."
+
+    def handle(self, *args, **kwargs):
+        system(
+            f"pg_dump --no-owner postgres://{getenv('DATABASE_USER')}:{getenv('DATABASE_PASSWORD')}@{getenv('DATABASE_HOST')}:{getenv('DATABASE_PORT')}/{getenv('DATABASE_NAME')} > dump.sql"
+        )
