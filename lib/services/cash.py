ea56534d98a63d6e73293dae5e1a0186de40a4cc
Marton Horvath <17813215+horvathmarton@users.noreply.github.com>
2022-03-16 20:34:02 +0100

    refactor: extract cash balance replay to SQL query

--- lib/services/cash.py
+++ lib/services/cash.py
@@ -8,6 +8,7 @@ from apps.raw_data.models import StockDividend
 from apps.stocks.models import StockPortfolio
 from apps.transactions.models import CashTransaction, ForexTransaction, StockTransaction
 from lib.dataclasses import CashBalanceSnapshot
+from lib.queries import sum_cash_transactions
 from lib.services.stocks import StocksService
 
 
@@ -29,18 +30,10 @@ class CashService:
 
         balance = CashBalanceSnapshot()
 
-        cash_transactions = CashTransaction.objects.filter(
-            portfolio__in=portfolios,
-            date__lte=snapshot_date,
-        )
-        forex_transactions = ForexTransaction.objects.filter(
-            portfolio__in=portfolios,
-            date__lte=snapshot_date,
-        )
-        stock_transactions = StockTransaction.objects.filter(
-            portfolio__in=portfolios,
-            date__lte=snapshot_date,
-        )
+        usd, eur, huf = sum_cash_transactions(portfolios, snapshot_date)
+        balance.USD = usd or 0
+        balance.EUR = eur or 0
+        balance.HUF = huf or 0
 
         owned_stocks = self.stocks_service.get_all_stocks_since_inceptions(
             portfolios, snapshot_date
@@ -56,43 +49,18 @@ class CashService:
             else []
         )
 
-        transactions = sorted(
-            [
-                *cash_transactions,
-                *forex_transactions,
-                *stock_transactions,
-                *dividend_payouts,
-            ],
-            key=lambda x: x.payout_date if isinstance(x, StockDividend) else x.date,
-        )
+        for transaction in dividend_payouts:
+            # pylint: disable=fixme
+            # TODO: This will be painfully slow for large dataset.
+            # TODO: We need to find a better way to calculate.
+            portfolio_snapshot = self.stocks_service.get_portfolio_snapshot(
+                portfolios, snapshot_date=transaction.payout_date
+            )
 
-        for transaction in transactions:
-            if isinstance(transaction, CashTransaction):
-                balance[transaction.currency] += transaction.amount
+            position = portfolio_snapshot.positions.get(transaction.ticker.ticker, None)
 
-            elif isinstance(transaction, ForexTransaction):
-                balance[transaction.source_currency] -= transaction.amount
-                balance[transaction.target_currency] += (
-                    transaction.amount * transaction.ratio
-                )
-
-            elif isinstance(transaction, StockTransaction):
-                balance.USD -= transaction.amount * transaction.price
-
-            elif isinstance(transaction, StockDividend):
-                # pylint: disable=fixme
-                # TODO: This will be painfully slow for large dataset.
-                # TODO: We need to find a better way to calculate.
-                portfolio_snapshot = self.stocks_service.get_portfolio_snapshot(
-                    portfolios, snapshot_date=transaction.payout_date
-                )
-
-                position = portfolio_snapshot.positions.get(
-                    transaction.ticker.ticker, None
-                )
-
-                if position:
-                    balance.USD += transaction.amount * position.shares
+            if position:
+                balance.USD += transaction.amount * position.shares
 
         return balance
 
