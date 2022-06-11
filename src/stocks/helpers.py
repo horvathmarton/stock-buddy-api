85355593dd8b641476309d55a598af7cdb3a842a
Marton Horvath <17813215+horvathmarton@users.noreply.github.com>
2022-06-11 12:31:29 +0200

    fix: don't send placeholder value when a watchlist is empty

--- src/stocks/helpers.py
+++ src/stocks/helpers.py
@@ -26,26 +26,29 @@ def parse_watchlist_rows(rows: list[WatchlistRow]) -> list[Watchlist]:
         )
 
         for ticker, inner_group in groupby(items, key=lambda x: x.stock_id):
-            targets = list(inner_group)
-            item = WatchlistItem(
-                ticker=ticker,
-                target_prices=[
-                    TargetPrice(target.target_name, cast(float, target.price))
-                    for target in targets
-                    if target.item_type == "target_price"
-                ],
-                position_sizes=[
-                    PositionSize(
-                        target.target_name,
-                        cast(float, target.size),
-                        cast(bool, target.at_cost),
-                    )
-                    for target in targets
-                    if target.item_type == "position_size"
-                ],
-            )
+            if ticker:
+                targets = list(inner_group)
+                item = WatchlistItem(
+                    ticker=ticker,
+                    target_prices=[
+                        TargetPrice(
+                            cast(str, target.target_name), cast(float, target.price)
+                        )
+                        for target in targets
+                        if target.item_type == "target_price"
+                    ],
+                    position_sizes=[
+                        PositionSize(
+                            cast(str, target.target_name),
+                            cast(float, target.size),
+                            cast(bool, target.at_cost),
+                        )
+                        for target in targets
+                        if target.item_type == "position_size"
+                    ],
+                )
 
-            watchlist.items.append(item)
+                watchlist.items.append(item)
 
         result.append(watchlist)
 
